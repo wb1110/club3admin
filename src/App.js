e82c8086d230a5c83e3cee3cde8b27a809c7d6bd
wb1110 <strengthcoachwb@gmail.com>
2022-01-06 08:30:40 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+// import { Admin, Resource, ListGuesser  } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import { UserList } from './users';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    {/*Replace ListGuesser when in production, https://marmelab.com/react-admin/Tutorial.html */}
+     {/* <Resource name="users" list={ListGuesser} />  */}
+     <Resource name="users" list={UserList} />
+  </Admin>
+);
 
 export default App;
