30ad18da16240b08293b87760e8be3a52f8b06b9
wb1110 <strengthcoachwb@gmail.com>
2022-01-06 11:32:54 -0600

    post component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 //import { Admin, Resource, ListGuesser  } from 'react-admin';
+// import { Admin, Resource, EditGuesser } from 'react-admin';
 import { Admin, Resource } from 'react-admin';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
-import { PostList } from "./posts";
+import { PostList, PostEdit, PostCreate } from "./posts";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
@@ -11,7 +12,9 @@ const App = () => (
     {/*Replace ListGuesser when in production, https://marmelab.com/react-admin/Tutorial.html */}
      {/* <Resource name="users" list={ListGuesser} />  */}
      {/* <Resource name="posts" list={ListGuesser} /> */}
-     <Resource name="posts" list={PostList} />
+     {/* <Resource name="posts" list={PostList} /> */}
+     {/* <Resource name="posts" list={PostList} edit={EditGuesser}/> */}
+     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
      <Resource name="users" list={UserList} />
   </Admin>
 );
--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,17 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+} from 'react-admin';
 
 export const PostList = props => (
     <List {...props}>
@@ -14,4 +26,35 @@ export const PostList = props => (
             <EditButton />
         </Datagrid>
     </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                {/* <SelectInput optionText="id" /> */}
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            {/* <TextInput source="id" /> */}
+            <TextInput source="title" />
+            {/* <TextInput source="body" /> */}
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                {/* <SelectInput optionText="id" /> */}
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            {/* <TextInput source="id" /> */}
+            <TextInput source="title" />
+            {/* <TextInput source="body" /> */}
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
 );
